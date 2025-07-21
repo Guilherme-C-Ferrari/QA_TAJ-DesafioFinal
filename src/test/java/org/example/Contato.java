b370e104c1aa91ce40ba43c251d2d7a77ff488e2
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2025-07-21 18:49:47 -0300

    Criação de Contato DTO.

---
+++ src/test/java/org/example/Contato.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Contato {
+    public String nome;
+    public String telefone;
+    public String email;
+    public String cpf;
+
+    public Contato(String n, String t, String e, String c) {
+        this.nome = n;
+        this.telefone = t;
+        this.email = e;
+        this.cpf = c;
+    }
+}
